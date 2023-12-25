fd9465dd1cd2dac3af618fbd3b26d04fc1395141
Rohit C <124383636+R021T@users.noreply.github.com>
2023-12-26 02:29:49 +0530

    Update urls.py

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import loginapi,signupapi,logoutapi,taskapi,expiredapi
+from .views import loginapi,signupapi,taskapi,expiredapi
 
 urlpatterns = [
     path('',loginapi.as_view()),
     path('signup/',signupapi.as_view()),
-    path('logout/',logoutapi.as_view()),
     path('task/',taskapi.as_view()),
     path('expired/',expiredapi.as_view())
 ]
